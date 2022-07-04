60938995a264a1850aa5fd0af0376cbd2bb836a1
Rick <medlin.rick@gmail.com>
2022-07-03 20:46:14 -0700

    More cleanup

--- cmd/web/routes_test.go
+++ cmd/web/routes_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"fsbb/internal/config"
 	"testing"
 
@@ -17,6 +16,6 @@ func TestRoutes(t *testing.T) {
 	case *chi.Mux:
 		// do nothing
 	default:
-		t.Error(fmt.Sprintf("type is not *chi.Mux, but is %T", v))
+		t.Errorf("type is not *chi.Mux, but is %T", v)
 	}
 }
