84b8f4d7969f1e5c47c3631aff4b2fb7bc7e8d56
Rick <medlin.rick@gmail.com>
2022-07-03 20:32:19 -0700

    Cleaned up render and handlers.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -87,7 +87,7 @@ func run() (*driver.DB, error) {
 
 	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
-	render.NewTemplates(&app)
+	render.NewRenderer(&app)
 	helpers.NewHelpers(&app)
 
 	return db, nil
