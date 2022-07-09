0747782e2e6f061804200cad1a9345be9a34b8c3
Rick <medlin.rick@gmail.com>
2022-07-08 21:05:15 -0700

    Added function to search all rooms.

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -77,7 +77,8 @@ func (m *postgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time,
 
 	query := `select count(id) from room_restrictions
 			  where room_id = $1
-			  and $2 < end_date and $3 > start_date;`
+			  and $2 < end_date
+			  and $3 > start_date;`
 
 	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
 	err := row.Scan(&numRows)
@@ -90,5 +91,47 @@ func (m *postgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time,
 	}
 
 	return false, nil
+
+}
+
+// SearchAvailabilityforAllRooms returns a slice of available rooms for a given date range.
+func (m *postgresDBRepo) SearchAvailabilityforAllRooms(start, end time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var rooms []models.Room
+
+	query := `select r.id, r.room_name
+			  from rooms r
+			  where r.id not in
+			  				(select rr.room_id
+							from room_restrictions rr
+							where '' < rr.end_date
+							and '' > rr.start_date);`
+
+	rows, err := m.DB.QueryContext(ctx, query, start, end)
+	if err != nil {
+		return rooms, err
+	}
+
+	for rows.Next() {
+		var room models.Room
+		err := rows.Scan(
+			&room.ID,
+			&room.RoomName,
+		)
+
+		if err != nil {
+			return rooms, err
+		}
+
+		rooms = append(rooms, room)
+	}
+
+	if err = rows.Err(); err != nil {
+		return rooms, err
+	}
+
+	return rooms, nil
 
 }
