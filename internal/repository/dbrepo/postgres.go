49001bfbf303566d9c02357828db85af32014fd5
Rick <medlin.rick@gmail.com>
2022-07-11 23:35:04 -0700

    Added GetRoomByID function.

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -135,3 +135,28 @@ func (m *postgresDBRepo) SearchAvailabilityforAllRooms(start, end time.Time) ([]
 	return rooms, nil
 
 }
+
+// GetRoomByID searches for a room by ID.
+func (m *postgresDBRepo) GetRoomByID(id int) (models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var room models.Room
+
+	query := `select id, room_name, created_at, updated_at
+			  from rooms where id = $1`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+	err := row.Scan(
+		&room.ID,
+		&room.RoomName,
+		&room.CreatedAt,
+		&room.UpdatedAt,
+	)
+
+	if err != nil {
+		return room, err
+	}
+
+	return room, nil
+}
