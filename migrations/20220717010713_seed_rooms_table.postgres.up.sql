ec72a77d161a77473f6a9d65a7bdf852cdb15e24
Rick <medlin.rick@gmail.com>
2022-07-16 18:15:36 -0700

    Added migrations for seeding rooms, restrictions.

---
+++ migrations/20220717010713_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Generals Quarters','2022-07-05 00:00:00.000','2022-07-05 00:00:00.000'),
+	 ('Majors Suite','2022-07-05 00:00:00.000','2022-07-05 00:00:00.000');
