6f40d99e68aac29974f94fd4c15c51761edc4d46
Rick <medlin.rick@gmail.com>
2022-07-16 18:45:28 -0700

    Updated tests, created test repo.

---
+++ internal/repository/dbrepo/test-repo.go
@@ -0,0 +1,45 @@
+package dbrepo
+
+import (
+	"fsbb/internal/models"
+	"time"
+)
+
+func (m *testDBRepo) AllUsers() bool {
+	return true
+}
+
+// insert a reservation into the database.
+func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
+
+	return 1, nil
+}
+
+// insert a room restriction into the database.
+func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+
+	return nil
+}
+
+// returns true if availability exists for room ID, false if no availability
+func (m *testDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+
+	return false, nil
+
+}
+
+// SearchAvailabilityforAllRooms returns a slice of available rooms for a given date range.
+func (m *testDBRepo) SearchAvailabilityforAllRooms(start, end time.Time) ([]models.Room, error) {
+
+	var rooms []models.Room
+	return rooms, nil
+
+}
+
+// GetRoomByID searches for a room by ID.
+func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
+
+	var room models.Room
+	return room, nil
+
+}
